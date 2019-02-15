5dc54ad73a73d3f25aeafacc4469e8a351b92c9f
Filip Pie <filipiusz@gmail.com>
2019-02-15 07:47:25 +0100

    first commit

---
+++ src/main/java/com/f/piechowiak/spring/PLDriver/PlDriverApplication.java
@@ -0,0 +1,14 @@
+package com.f.piechowiak.spring.PLDriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlDriverApplication.class, args);
+	}
+
+}
+
